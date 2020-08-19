c3f1b71bca8f9a9d74ff5abafc2d7017ba09e771
johnratius <jasonmemm@gmail.com>
2020-08-19 15:04:03 -0700

    Fixed premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,33 @@
 workspace "MyProject"
 	architecture "x86_64"
-	startproject "MyProject"
 	configurations { "Debug", "Release" }
 
-    	outputdir "%{cfg.config}-%{cfg.platform}-%{cfg.architecture}"
+build_dir = "%{cfg.config}-%{cfg.platform}-%{cfg.architecture}"
 
 project "MyProject"
-    location "MyProject"
-    language "C++"
-    kind "ConsoleApp"
-    targetdir "bin/" .. builddir .. "/%{prj.name}"
-    objdir "bin/" .. builddir .. "/%{prj.name}"
+	location "MyProject"
+	language "C++"
+	kind "ConsoleApp"
+	targetdir ("bin/" .. build_dir .. "/%{prj.name}")
+	objdir ("bin/" .. build_dir .. "/%{prj.name}")
 
-    files { "**.h", "**.hpp", "**.c", "**.cpp" }
+	files { "**.h", "**.hpp", "**.c", "**.cpp" }
 
-    includedirs { "%{prj.name}/src" }
+	includedirs { "%{prj.name}/src" }
 
-    filter "configuration:Debug"
-        defines { "_CONFIG_DEBUG" }
-        symbols "On"
+	filter "configurations:Debug"
+		defines { "_CONFIG_DEBUG" }
+		symbols "On"
 
-    filter "configuration:Release"
-        defines { "_CONFIG_RELEASE" }
-        optimize "On"
+	filter "configurations:Release"
+		defines { "_CONFIG_RELEASE" }
+		optimize "On"
 
-    filter "system:windows"
-        defines { "_SYSTEM_WINDOWS" }
+	filter "system:windows"
+		defines { "_SYSTEM_WINDOWS" }
 
-    filter "system:linux"
-        defines { "_SYSTEM_LINUX" }
+	filter "system:linux"
+		defines { "_SYSTEM_LINUX" }
 
-    filter "system:macosx"
-        defines { "_SYSTEM_MACOSX" }
+	filter "system:macosx"
+		defines { "_SYSTEM_MACOSX" }
