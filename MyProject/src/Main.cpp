58cad064eb200effc54c172465945520ad3de7c7
BEAST <49040508+BEASTSM96@users.noreply.github.com>
2020-08-22 20:02:36 +0100

    Update Main.cpp

--- MyProject/src/Main.cpp
+++ MyProject/src/Main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello, World!" << std::endl;
+    //App Loop
+    while true()
+        
+    //std::cin.get();
+    //return 0;
 
-    std::cin.get();
-    return 0;
 }
